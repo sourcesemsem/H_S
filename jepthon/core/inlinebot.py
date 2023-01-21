17c70e90d5190b435c23fb5d851094de541273be
sourcesemsem <122203365+sourcesemsem@users.noreply.github.com>
2023-01-21 13:58:31 +0200

    Update inlinebot.py

--- jepthon/core/inlinebot.py
+++ jepthon/core/inlinebot.py
@@ -53,7 +53,7 @@ def ibuild_keyboard(buttons):
 
 
 def main_menu():
-    text = f"**- [سـورس الجوكر](https://t.me/JepThon)\
+    text = f"**- [سـورس سـيـمـو](https://t.me/FTTUTY)\
         \n المسـاعد\
         \n\
         \n◽ الجوكر لـ {mention}**"
@@ -228,7 +228,7 @@ async def inline_handler(event):  # sourcery no-metrics
             buttons = [
                 (
                     Button.inline("الحـالة ⚒️", data="stats"),
-                    Button.url(" Jepthon UsᴇʀBoᴛ", "https://t.me/Jepthon"),
+                    Button.url(" semo UsᴇʀBoᴛ", "https://t.me/FTTUTY"),
                 )
             ]
             ALIVE_PIC = gvarstatus("ALIVE_PIC")
@@ -336,7 +336,7 @@ async def inline_handler(event):  # sourcery no-metrics
 
             buttons = [Button.inline("عـرض الـرسـالة 🔐", data=f"troll_{timestamp}")]
             result = builder.article(
-                title="رسـالة تـرول من سـورس الجوكر 🧸♥",
+                title="رسـالة تـرول من سـورس سـيـمـو🧸♥",
                 text=f"فقـط {razan} هـو مـن يستـطيع مشـاهدتهـا !",
                 buttons=buttons,
             )
@@ -386,7 +386,7 @@ async def inline_handler(event):  # sourcery no-metrics
 
             buttons = [Button.inline("عـرض الـرسـالة 🔐", data=f"rzan_{timestamp}")]
             result = builder.article(
-                title="همسـة سـرية من سـورس الجوكر 🧸♥",
+                title="همسـة سـرية من سـورس سيمو 🧸♥",
                 text=f"فقـط {razan} هـو مـن يستـطيع مشـاهدتهـا !",
                 buttons=buttons,
             )
@@ -409,7 +409,7 @@ async def inline_handler(event):  # sourcery no-metrics
 
             buttons = [Button.inline("قراءة الـرسالـة ", data=f"hide_{timestamp}")]
             result = builder.article(
-                title="رسـالة مخفيـة من سـورس الجوكر 🧸♥",
+                title="رسـالة مخفيـة من سـورس سيمو 🧸♥",
                 text=f"✖✖✖",
                 buttons=buttons,
             )
@@ -559,10 +559,10 @@ async def inline_handler(event):  # sourcery no-metrics
     else:
         buttons = [
             (
-                Button.url("قنـاة السـورس ⚒️", "https://t.me/Jepthon"),
+                Button.url("قنـاة السـورس ⚒️", "https://t.me/FTTUTY"),
                 Button.url(
-                    "كـروب المـساعدة 📬",
-                    "https://t.me/Jepthon1",
+                    "جـروب المـساعدة 📬",
+                    "https://t.me/FTTUTT3",
                 ),
             )
         ]
@@ -571,20 +571,20 @@ async def inline_handler(event):  # sourcery no-metrics
             url=ROZLOGO, size=0, mime_type="image/jpeg", attributes=[]
         )
         text, msg_entities = await event.client._parse_message_text(
-            "**[بـوت الجوكر 🧸♥](https://t.me/Jepthon)**\
+            "**[بـوت سيمو 🧸♥](https://t.me/FTTUTY)**\
             \n\
-            \n❤ الجوكر هـو بـوت بسـيط يدخـل الـى حسـابك لجعـلك تتحـكم به معـ اوامـر معينـة.\
+            \n❤ سيمو هـو بـوت بسـيط يدخـل الـى حسـابك لجعـلك تتحـكم به معـ اوامـر معينـة.\
             \n\
             \n**اذا كـنت تـرغب بتـنصيـب السـورس عـلى حسـابك ,\
-            \n🐾 تعال الـى [كروب المسـاعدة](https://t.me/Jepthon1)!**",
+            \n🐾 تعال الـى [جروب المسـاعدة](https://t.me/FTTUTT3)!**",
             "md",
         )
         result = types.InputBotInlineResult(
             id=str(uuid4()),
             type="photo",
             title="jepiq 🧸♥",
-            description="ادخـل كـروب المسـاعدة",
-            url="https://t.me/Jepthon1",
+            description="ادخـل جـروب المسـاعدة",
+            url="https://t.me/FTTUTT3",
             thumb=photo,
             content=photo,
             send_message=types.InputBotInlineMessageMediaAuto(
